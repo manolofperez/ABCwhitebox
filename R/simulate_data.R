29fc0e4e6480e3a37cd059af64fcc70315dfd17a
manolofperez <manolofperez@gmail.com>
2017-03-30 16:54:11 -0300

    Add files via upload

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -1,11 +1,14 @@
 library(phyclust)
 library(PopGenome)
-library(doParallel)
+library(foreach)
+dir.create("step1")
+
 dir.create("temp")
 
-n.cores <- parallel::detectCores() #multicore:::detectCores()
-n.cores <- n.cores - 1 
-registerDoParallel(n.cores)
+n.cores <- detectCores() #multicore:::detectCores()
+n.cores <- n.cores - 7 
+cl<-makeCluster(n.cores)
+registerDoParallel(cl)
 ## number of simulations
 numsim <- 2
 
@@ -138,26 +141,31 @@ sample_priors <- function(model_number){
 #' (nuclear and/or cytoplasmatic)
 #' @return A table with the calculated summary statistics
 #' @export
-simulate_seqs <- function(seq_genome){
+simulate_seqs <- function(nDNA, cpDNA){
   if (nDNA == T){
-    write(ms(nsam=Nsam, nreps=1, opts= paste(nc_modelstr, seq_model_opt[[model]]), tbs.matrix=rbind2(nc_tbs_values[rep,])),file=file.path("temp",sprintf("ncsim_%d_%09.f.txt", model, rep)))
-    nc_ms<-readMS(file.path("temp",sprintf("ncsim_%d_%09.f.txt", model, rep)))
-    nc_ms<-set.populations(nc_ms,list(c(1:Pop1),c((Pop1+1):(Pop1+Pop2))))
-    nc_ms<-F_ST.stats(nc_ms)
-    nc_ms<-diversity.stats(nc_ms)
-    nc_ms<-neutrality.stats(nc_ms)
-    nss<-c(nc_ms@n.segregating.sites,nc_ms@nuc.diversity.within, nc_ms@nuc.diversity.between, nc_ms@Tajima.D, nc_ms@theta_Watterson, nc_ms@Hudson.G_ST,nc_ms@hap.F_ST.pairwise)
-    ncss_table<<-rbind(ncss_table, nss)
+    for (nloc in 1:Nnloci){
+      write(ms(nsam=Nsam, nreps=1, opts= paste(nc_modelstr, seq_model_opt[[model]]), tbs.matrix=rbind2(nc_tbs_values[rept,])),file=file.path("temp","ncsim.txt"))
+      nc_ms<-readMS(file.path("temp","ncsim.txt"))
+      nc_ms<-set.populations(nc_ms,list(c(1:Pop1),c((Pop1+1):(Pop1+Pop2))))
+      nc_ms<-F_ST.stats(nc_ms)
+      nc_ms<-diversity.stats(nc_ms)
+      nc_ms<-neutrality.stats(nc_ms)
+      nss<-c(nc_ms@n.segregating.sites,nc_ms@nuc.diversity.within, nc_ms@nuc.diversity.between, nc_ms@Tajima.D, nc_ms@theta_Watterson, nc_ms@Hudson.G_ST,nc_ms@hap.F_ST.pairwise)
+      ncss_table<<-rbind(ncss_table, nss)
+    }
+    
   }
   if (ctDNA == T){
-    write(ms(nsam=Nsam, nreps=1, opts= paste(ct_modelstr, seq_model_opt[[model]]), tbs.matrix=rbind2(ct_tbs_values[rep,])),file=file.path("temp",sprintf("ctsim_%d_%09.f.txt", model, rep)))
-    ct_ms<-readMS(file.path("temp",sprintf("ctsim_%d_%09.f.txt", model, rep)))
-    ct_ms<-set.populations(ct_ms,list(c(1:Pop1),c((Pop1+1):(Pop1+Pop2))))
-    ct_ms<-F_ST.stats(ct_ms)
-    ct_ms<-diversity.stats(ct_ms)
-    ct_ms<-neutrality.stats(ct_ms)
-    css<-c(ct_ms@n.segregating.sites,ct_ms@nuc.diversity.within, ct_ms@nuc.diversity.between, ct_ms@Tajima.D, ct_ms@theta_Watterson, ct_ms@Hudson.G_ST,ct_ms@hap.F_ST.pairwise)
-    ctss_table<<-rbind(ctss_table, css)
+    for (ctloc in 1:Nctloci){
+      write(ms(nsam=Nsam, nreps=1, opts= paste(ct_modelstr, seq_model_opt[[model]]), tbs.matrix=rbind2(ct_tbs_values[rept,])),file=file.path("temp","ctsim.txt"))
+      ct_ms<-readMS(file.path("temp","ctsim.txt"))
+      ct_ms<-set.populations(ct_ms,list(c(1:Pop1),c((Pop1+1):(Pop1+Pop2))))
+      ct_ms<-F_ST.stats(ct_ms)
+      ct_ms<-diversity.stats(ct_ms)
+      ct_ms<-neutrality.stats(ct_ms)
+      css<-c(ct_ms@n.segregating.sites,ct_ms@nuc.diversity.within, ct_ms@nuc.diversity.between, ct_ms@Tajima.D, ct_ms@theta_Watterson, ct_ms@Hudson.G_ST,ct_ms@hap.F_ST.pairwise)
+      ctss_table<<-rbind(ctss_table, css)
+    }
   }
 }
 
@@ -170,18 +178,13 @@ for(model in 1:Nmodels) {
   ct_tbs_values<-cbind(theta_ctloci,tbs_values)
   nc_tbs_values<-cbind(theta_nloci,tbs_values)
   #foreach (rep=1:numsim, .packages=c('phyclust','PopGenome')) %dopar%{
-  for (rep in 1:numsim){
-    for (nloc in 1:Nnloci){
-      simulate_seqs(nDNA)
-    }
-    for (ctloc in 1:Nctloci){
-      simulate_seqs(ctDNA)
-    }
+  for (rept in 1:numsim){
+      simulate_seqs(nDNA, ctDNA)
   }
-  
 }
+unlink("temp",recursive=TRUE)
 
-##to runs microsat data (TO DO
+##to runs microsat data (TO DO)
 #  for(simulation in 1:numsim){
 #  simd_data <- sim_microsats(theta = 1,
 #                             n_ind = c(73,20),
@@ -190,92 +193,37 @@ for(model in 1:Nmodels) {
 #                             ms_options = "-I 2 146 40 -n 1 tbs -n 2 tbs -ej tbs 1 2",
 #                             tbs_matrix = cbind(0.5,0.5,2.3))
 #  }
-#unlink("temp",recursive=TRUE)
+#
 
-#nc_ms.txt<-readms(nc_ms)
-#lapply(tbs_values[simulation,], function(tbs){seq_model_opt[[1]]})
+### calculate summary statistics mean the simulated loci
+ncss_avg <- function(ncss_table){
+  ncavgss_table = c()
+  for (sust in 1:length(ncss_table[1,])){
+    ncavgss_table<-cbind(ncavgss_table, tapply(ncss_table[,sust], factor(rep(1:(numsim*Nmodels), each=Nnloci)), mean,na.rm=T))
+  }
+  return(ncavgss_table)
+}
 
-#    ## use the DivTime in years to calculte divergence time in colaescent units (required by ms)
-#
-#    ## ms's command
-#    system(sprintf("./ms %d %d -t %f -I 2 %f %f -ej %f 1 2 | ./sample_stats >> const.txt", Nsam, numloc, theta, Pop1, Pop2, coalDivTime))
-#
-#    ## save parameter values
-#    parameters <- rbind(parameters, data.frame(Ne, DivTime, RatioPop1, RatioPop2, Migration12, Migration21))
-#  }
-#}
-#
-### bottleneck in both pops model
-#for (i in 1:numsim) {
-#
-#  ### Define parameters
-#  Ne <- runif(1, 100, 30000)
-#  theta <- Ne*4*mutrate*L
-#  DivTime <- runif(1, 300000, 4000000)
-#  coalDivTime <- DivTime/(genlen*4*Ne)
-#  ##Bottleneck intensity in each population, measured as a rate of the Ne in the original population. Prior sampled from an uniform distribution from 0.01 to 1.
-#  RatioPop1 <- runif(1, 0.01, 1)
-#  RatioPop2 <- runif(1, 0.01, 1)
-#
-#  ## ms's command
-#  system(sprintf("./ms %d %d -t %f -I 2 %f %f -n 1 %f -n 2 %f -ej %f 1 2 | ./sample_stats >> bott.txt", Nsam, numloc, theta, Pop1, Pop2, RatioPop1, RatioPop2, coalDivTime))
-#
-#  ## save parameter values
-#  parameters <- rbind(parameters, data.frame(Ne, DivTime, RatioPop1, RatioPop2, Migration12, Migration21))
-#}
-#
-### Bidirectional migration model
-#for (i in 1:numsim) {
-#
-#  ### Define parameters
-#  Ne <- runif(1, 100, 30000)
-#  theta <- Ne*4*mutrate*L
-#  DivTime <- runif(1, 300000, 4000000)
-#  coalDivTime <- DivTime/(genlen*4*Ne)
-#  ## migration prior sampled sampled from an uniform distribution from 0.1 to 5 migrants per generation.
-#  Migration12 <- runif(1, 0.1, 5)
-#  Migration21 <- runif(1, 0.1, 5)
-#
-#  ## ms's command
-#  system(sprintf("./ms %d %d -t %f -I 2 %f %f -m 1 2 %f -m 2 1 %f -ej %f 1 2 | ./sample_stats >> migr.txt", Nsam, numloc, theta, Pop1, Pop2, Migration12, Migration21, coalDivTime))
-#
-#  ## save parameter values
-#  parameters <- rbind(parameters, data.frame(Ne, DivTime, RatioPop1, RatioPop2, Migration12, Migration21))
-#}
-#
-###sample_stats exports data in the following format: pi:	0.404999	ss:	1	D:	1.381008	thetaH:	0.157164	H:	0.247835
-### calculate summary statistics mean and variance over the 25 loci
-#const <- read.table("const.txt")
-#const <- data.frame(pi.m=tapply(const[,2]/L, factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                    ss.m=tapply(const[,4], factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                    D.m=tapply(const[,6], factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                    T.m=tapply(const[,8], factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                    TH.m=tapply(const[,10], factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                    pi.v=tapply(const[,2]/L, factor(rep(1:numsim, each=numloc)), var,na.rm=T),
-#                    ss.v=tapply(const[,4], factor(rep(1:numsim, each=numloc)), var,na.rm=T),
-#                    D.v=tapply(const[,6], factor(rep(1:numsim, each=numloc)), var,na.rm=T))
-#bott <- read.table("bott.txt")
-#bott <- data.frame(pi.m=tapply(bott[,2]/L, factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                   ss.m=tapply(bott[,4], factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                   D.m=tapply(bott[,6], factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                   T.m=tapply(bott[,8], factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                   TH.m=tapply(bott[,10], factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                   pi.v=tapply(bott[,2]/L, factor(rep(1:numsim, each=numloc)), var,na.rm=T),
-#                   ss.v=tapply(bott[,4], factor(rep(1:numsim, each=numloc)), var,na.rm=T),
-#                   D.v=tapply(bott[,6], factor(rep(1:numsim, each=numloc)), var,na.rm=T))
-#migr <- read.table("migr.txt")
-#migr <- data.frame(pi.m=tapply(migr[,2]/L, factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                   ss.m=tapply(migr[,4], factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                   D.m=tapply(migr[,6], factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                   T.m=tapply(migr[,6], factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                   TH.m=tapply(migr[,6], factor(rep(1:numsim, each=numloc)), mean,na.rm=T),
-#                   pi.v=tapply(migr[,2]/L, factor(rep(1:numsim, each=numloc)), var,na.rm=T),
-#                   ss.v=tapply(migr[,4], factor(rep(1:numsim, each=numloc)), var,na.rm=T),
-#                   D.v=tapply(migr[,6], factor(rep(1:numsim, each=numloc)), var,na.rm=T))
-#
-#models <- rep(c("const", "bott", "migr"), each=numsim)
-#
+ctss_avg <- function(ctss_table){
+  ctavgss_table = c()
+  for (sust in 1:length(ctss_table[1,])){
+    ctavgss_table<-cbind(ctavgss_table, tapply(ctss_table[,sust], factor(rep(1:(numsim*Nmodels), each=Nnloci)), mean,na.rm=T))
+  }
+  return(ctavgss_table)
+}
+
 ### join data
+
+ncss_table = ncss_avg(ncss_table)
+ctss_table = ctss_avg(ctss_table)
+ss_table = cbind(ncss_table, ctss_table)
+
+##enter the models as a vector
+models=c()
+for (m in 1:Nmodels){
+  models<-c(models, rep(m, each=numsim))
+}
+#
 #sust <- rbind(const, bott, migr)
 #names(sust) <- c("pi.m", "ss.m", "TajD.m", "T.m", "TH.m","pi.v", "ss.v","TajD.v")
 #
